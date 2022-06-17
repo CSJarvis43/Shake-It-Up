b74cad98e02d480791d5f4a293db3d9149ce5ca5
Charles Jarvis <csjarvis43@gmail.com>
2022-06-17 16:03:34 -0600

    some work on main render function done. small html changes

--- index.js
+++ index.js
@@ -7,22 +7,44 @@ function init(){
     .then(drinkData => drinkData.drinks.forEach(renderCocktails))
 }
 
+function initialFocus(){
+    fetch(baseUrl + '/random.php')
+    .then(res => res.json())
+    .then(drinkData => {
+        const drink = drinkData.drinks
+        renderMain(drink)
+    })
+}
+
 function renderCocktails(drinks){
     const card = document.createElement('div')
     card.id = 'drink_card'
 
     const name = document.createElement('h2')
     name.textContent = drinks.strDrink
-    console.log(name)
+    // console.log(name)
 
     const image = document.createElement('img')
     image.src = drinks.strDrinkThumb
     image.details = drinks
-    console.log(image.details)
+    // console.log(image.details)
 
     card.append(name, image)
     drinkCollection.append(card)
 
 }
 
+function renderMain(drink){
+    const drinkName = document.getElementById('focus_name')
+    drinkName.innerText = drink[0].strDrink
+
+    const drinkImage = document.getElementById('drink_focus')
+    drinkImage.src = drink[0].strDrinkThumb
+
+    const drinkInstructions = document.getElementById('instructions')
+    drinkInstructions.innerText = drink[0].strInstructions
+
+}
+
 init()
+initialFocus()
