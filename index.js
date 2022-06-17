4b4c52f3ad9fcb1ec276792ce263ed42ca9e7b33
Charles Jarvis <csjarvis43@gmail.com>
2022-06-17 15:47:57 -0600

    basic fetch request and card render function finished. some html also added

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1'
+const drinkCollection = document.getElementById('drink_collection')
 
 function init(){
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    fetch(baseUrl + '/filter.php?i=Vodka')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(drinkData => drinkData.drinks.forEach(renderCocktails))
+}
+
+function renderCocktails(drinks){
+    const card = document.createElement('div')
+    card.id = 'drink_card'
+
+    const name = document.createElement('h2')
+    name.textContent = drinks.strDrink
+    console.log(name)
+
+    const image = document.createElement('img')
+    image.src = drinks.strDrinkThumb
+    image.details = drinks
+    console.log(image.details)
+
+    card.append(name, image)
+    drinkCollection.append(card)
+
 }
 
 init()
