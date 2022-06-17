52734c26ccb99a1573032aa12d99ca083c416d50
Charles Jarvis <csjarvis43@gmail.com>
2022-06-17 14:53:48 -0600

    finished api test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const baseUrl = 'http://www.thecocktaildb.com/api/json/v1/1'
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1'
 
 function init(){
-    fetch('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
     .then(res => res.json())
     .then(data => console.log(data))
 }
